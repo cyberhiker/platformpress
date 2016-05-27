367443fa72116bf97dfd701ebc0dc824e909e058
Ubuntu <ubuntu@ip-172-30-2-217.us-west-2.compute.internal>
2016-05-27 20:48:06 +0000

    Fixing deploy.php

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@
 	$commands = array(
 		'echo $PWD',
 		'whoami',
-		'git pull https://github.com/cyberhiker/platformpress.git dev',
+		'git pull',
 		'git status',
 		'git submodule sync',
 		'git submodule update',
