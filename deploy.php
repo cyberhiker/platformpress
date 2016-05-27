23a374d2967a26812f26e19e2172dc91233f52cf
Chris Burton <cyberhiker@gmail.com>
2016-05-27 16:14:48 -0400

    Pull Command fix

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@
 	$commands = array(
 		'echo $PWD',
 		'whoami',
-		'git pull',
+		'git pull https://github.com/cyberhiker/platformpress.git dev',
 		'git status',
 		'git submodule sync',
 		'git submodule update',
