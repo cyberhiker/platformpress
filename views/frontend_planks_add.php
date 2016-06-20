90acd05fc7b8bcb00f9fe772a2ba0e7fd2694c9e
Chris Burton <cburton@redeyetek.com>
2016-06-20 13:47:02 -0400

    Initial add of Topics

--- views/frontend_planks_add.php
+++ views/frontend_planks_add.php
@@ -10,18 +10,18 @@
 	<h2>Add new plank</h2>
 	<?php endif; ?>
 	 <?php require_once(PLATFORMPRESS_PLUGIN_INCLUDE_PATH.'platformpress-flash-messages.php'); ?>
-	
+
 	<?php if(!$this->isUserAllowed(get_current_user_id(),'can_ask_planks')) : ?>
-	
+
 		<div class="no-access">
 		<h4>Access not allowed</h4>
 		Sorry, you are not allowed to ask plank
 		</div>
-	
+
 	<?php else: ?>
 	<?php
 	# relative current URI:
-	$url = add_query_arg( NULL, NULL );	
+	$url = add_query_arg( NULL, NULL );
 	?>
 	<div class="form_plank_wrapper"><form id="platformpressform" name="platformpressform" method="post" action="<?php echo $url; ?>">
 
@@ -29,33 +29,55 @@
 		<label>Plank title</label>
 		<input id="plank_title" type="text"  aria-required="true" value="<?php echo (isset($_POST['plank_title'])) ? $_POST['plank_title'] : ""; ?>" name="plank_title">
 		</div>
-		
+
 		<br />
 		<div class="input-row">
-			<label>Plank description</label>		
-			<?php 
+			<label>Plank description</label>
+			<?php
 			//Remark textarea
-			platformpress_editor( array( 
+			platformpress_editor( array(
 				'content' => ( isset( $_POST['plank_description'] ) ? $_POST['plank_description'] : '' ),
-				'id' 			=> 'plank_description', 
+				'id' 			=> 'plank_description',
 				'textarea_name' => 'plank_description',
 			));
 			?>
 		</div>
-		
+
+        <li id="categories">
+        	<label>Plank Category</label>
+        	<form id="category-select" class="category-select" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+
+        		<?php
+        		$args = array(
+        			'show_option_none' => __( 'Select category' ),
+        			'show_count'       => 1,
+        			'orderby'          => 'name',
+        			'echo'             => 0,
+        		);
+        		?>
+
+        		<?php $select  = wp_dropdown_categories( $args ); ?>
+        		<?php $replace = "<select$1 onchange='return this.form.submit()'>"; ?>
+        		<?php $select  = preg_replace( '#<select([^>]*)>#', $replace, $select ); ?>
+
+        		<?php echo $select; ?>
+
+
+        	</form>
+        </li>
 		<?php if((!current_user_can( 'manage_options' ))): ?>
 		<?php if((platformpress_setting_get("auto_approve_new_planks")=="0")): ?>
 		<p>Note: Your plank will reviewed before publishing.</p>
 		<?php endif; ?>
 		<?php endif; ?>
-		
+
 		<div class="input-row platformpress_submit_button">
 			<input type="submit" value="Submit" id="submit" />
 		</div>
 	</form></div>
 	<?php endif; ?>
-	
-</div>	
+
+</div>
 <?php
 	wp_enqueue_script(
 		'jquery-validate',
@@ -77,5 +99,5 @@
 		array('jquery'),
 		'1.10.0',
 		true
-	);	
+	);
 ?>
